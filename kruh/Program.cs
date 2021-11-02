9b58b8811742a70a423f974359926889a3c4f3e1
Šimon Jurdič <Jurdic.Simon@stud.vassboskovice.cz>
2021-11-02 08:18:26 +0100

    Přidejte soubory projektu.

---
+++ kruh/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace kruh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
